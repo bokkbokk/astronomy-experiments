fed71e7d4f9721c32f1892f87193d81197903e6f
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2025-09-16 13:38:05 +0000

    html

---
+++ html/3d.js
@@ -0,0 +1,13 @@
+function init(){
+    while(true){
+        var stage = new createjs.Stage("canvas");
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+        circle.x = 100;
+        circle.y = 100;
+        stage.addChild(circle);
+
+
+        stage.update();
+    }
+}
